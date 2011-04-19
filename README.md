7e6a058f6e953fcc28816d312da72a397df70a90
Lele <lele@amicofigo.com>
2011-04-20 01:40:27 +0200

    README converted in md format

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTTPFileServer 
+## Description
+  Simple HTTPFileServer done in a single night
+## Disclaimer
+  **WARNING**
+  This is a simple first project of a nodeJS n00b, trying to be less a dev n00b.
+  
---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var fs = require('fs');
+var HTTP_port;
+var app = express.createServer();
+
+app.configure('dev', function(){
+  HTTP_port = 60080 ;
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.logger(),function(){
+    
+    });
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+app.configure('prod', function(){
+  var oneYear = 31557600000;
+  HTTP_port = 80 ;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('views');
+  // => "/absolute/path/to/views"
+  app.set('view engine', 'jade');
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+app.get('/',function(req,res){
+  var filesDir = './';
+  fs.readdir(filesDir,function(err,files){
+    return res.render('root', { files : files });
+  });
+});
+
+try {
+  if (HTTP_port) {
+    app.listen(HTTP_port);
+  } else {
+    throw "No working env specified .. use \"NODE_ENV='prod' node app.js\" or \"NODE_ENV='dev' node app.js\""
+  }
+} catch (err) {
+  console.log("Error: " + err);
+};
